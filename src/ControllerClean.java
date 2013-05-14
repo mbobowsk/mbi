6c9ba2e1616c88e3ba971efc7a30484210207c23
Michał Bobowski <m_bobowski@o2.pl>
2013-05-14 13:23:51 +0200

    Controller w 3D.

--- src/ControllerClean.java
+++ src/ControllerClean.java
@@ -10,39 +10,43 @@ public class ControllerClean {
 	public HashSet<Tuple> result = new HashSet<Tuple>();
 
 	public void findSimilarity(String first, String second, String third, int offsetX, int offsetY, int offsetZ) {
-		// Build complete matrix
-		if (first.length() < 12 || second.length() < 12 || third.length() < 12) {
-			int cols = first.length() + 1;
-			int rows = second.length() + 1;
-			int depth = third.length() + 1;
+		int cols = first.length() + 1;
+		int rows = second.length() + 1;
+		int depth = third.length() + 1;
+		
+		// Build full matrix
+		if (first.length() < 2 || second.length() < 2 || third.length() < 2) {
 			Cell matrix[][][] = new Cell[cols][rows][depth];
 
 			// Initialize matrix
 			for (int i = 0; i < cols; i++) {
-				matrix[i][0][0] = new Cell(i*model.c);
+				matrix[i][0][0] = new Cell(i*model.c, Directions.DIRECTION_LEFT);
 			}
 			for (int i = 0; i < rows; i++) {
-				matrix[0][i][0] = new Cell(i*model.c);
+				matrix[0][i][0] = new Cell(i*model.c, Directions.DIRECTION_UP);
 			}
 			for (int i = 0; i < depth; i++) {
-				matrix[0][0][i] = new Cell(i*model.c);
+				matrix[0][0][i] = new Cell(i*model.c, Directions.DIRECTION_RIGHT);
 			}
 
 			for ( int i = 1; i < rows; i++ ) {
 				for ( int j = 1; j < depth; j++ ) {
-					int cross = matrix[0][i-1][j-1].getScore() + match('-',second.charAt(i-1),third.charAt(j-1));
-					int gapVertical = matrix[0][i-1][j].getScore() + model.gap;
-					int gapHorizontal = matrix[0][i][j-1].getScore() + model.gap;
-					int max = Math.max(Math.max(cross,gapVertical),gapHorizontal);
-					matrix[0][i][j] = new Cell(max);
+					int rightCross = matrix[0][i-1][j-1].getScore() + match('-',second.charAt(i-1),third.charAt(j-1));
+					int up = matrix[0][i-1][j].getScore() + model.gap;
+					int right = matrix[0][i][j-1].getScore() + model.gap;
+					int max = Math.max(Math.max(rightCross, up), right);
+					if (max==rightCross)
+						matrix[0][i][j] = new Cell(max, Directions.DIRECTION_RIGHTCROSS);
+					else if (max==up)
+						matrix[0][i][j] = new Cell(max, Directions.DIRECTION_UP);
+					else // gapHorizontal
+						matrix[0][i][j] = new Cell(max, Directions.DIRECTION_RIGHT);
 				}
 			}
-
 
 			for (int i = 1; i < cols; i++) {
 				for (int j = 0; j < rows; j++) {
 					for (int k = 0; k < depth; k++) {
-
 						if (j==0 && k==0) { // already initialized
 							continue;
 						}
@@ -76,7 +80,6 @@ public class ControllerClean {
 								matrix[i][j][k] = new Cell(max, Directions.DIRECTION_UP);
 							}
 						} else {
-
 							// DIRECTION_CROSS
 							int cross = matrix[i-1][j][k-1].getScore() + match(first.charAt(i-1), '-', third.charAt(k-1));
 							// DIRECTION_LEFT
@@ -156,24 +159,161 @@ public class ControllerClean {
 					break;
 				}
 			}
-		} else {
-			// Divide problem
+		} else { // Divide and conquer
+			// Beginning of index propagation
+			int x = cols/2;
 
+			// Parts of matrix
+			Cell col1[][] = new Cell[rows][depth];
+			Cell col2[][] = new Cell[rows][depth];
+
+			// Initialize col1
+			for (int i = 0; i < rows; i++) {
+				col1[i][0] = new Cell(i*model.c);
+			}
+			for (int i = 0; i < depth; i++) {
+				col1[0][i] = new Cell(i*model.c);
+			}
+			for (int i = 1; i < rows; i++) {
+				for (int j = 1; j < depth; j++) {
+					int cross = col1[i-1][j-1].getScore() + match(second.charAt(i-1), third.charAt(j-1));
+					int gapVertical = col1[i-1][j].getScore() + model.gap;
+					int gapHorizontal = col1[i][j-1].getScore() + model.gap;
+					int max = Math.max(Math.max(cross, gapVertical), gapHorizontal);
+					col1[i][j] = new Cell(max);
+				}
+			}
+
+			
+			for (int i = 1; i < cols; i++) {
+				// Create and fill col2 based on col1
+				col2 = new Cell[rows][depth];
+				for (int j = 0; j < rows; j++) {
+					for (int k = 0; k < depth; k++) {
+						if ( j == 0 && k == 0 )
+							col2[0][0] = new Cell(i*model.c, 0, 0);
+						else if (j == 0 && k != 0) {
+							// top wall
+							int cross = col1[j][k-1].getScore() + match(first.charAt(i-1), third.charAt(k-1));
+							int gapVertical = col2[j][k-1].getScore() + model.gap;
+							int gapHorizontal = col1[j][k].getScore() + model.gap;
+							int max = Math.max(Math.max(cross, gapVertical), gapHorizontal);
+
+							if (i <= x)
+								col2[j][k] = new Cell(max,j,k);
+							else {
+								int index, index2;
+								if (cross == max) {
+									index = col1[j][k-1].getIndex();
+									index2 = col1[j][k-1].getIndex2();
+								}
+								else if (gapHorizontal == max) {
+									index = col1[j][k].getIndex();
+									index2 = col1[j][k].getIndex2();
+								}
+								else {
+									index = col2[j][k-1].getIndex();
+									index2 = col2[j][k-1].getIndex2();
+								}
+								col2[j][k] = new Cell(max, index, index2);
+							}				
+
+						}
+						else if (j != 0 && k == 0) {
+							// front wall
+							int cross = col1[j-1][k].getScore() + match(first.charAt(i-1), second.charAt(j-1));
+							int gapVertical = col2[j-1][k].getScore() + model.gap;
+							int gapHorizontal = col1[j][k].getScore() + model.gap;
+							int max = Math.max(Math.max(cross, gapVertical), gapHorizontal);						
+
+							if (i <= x)
+								col2[j][k] = new Cell(max,j,k);
+							else {
+								int index, index2;
+								if (cross == max) {
+									index = col1[j-1][k].getIndex();
+									index2 = col1[j-1][k].getIndex2();
+								}
+								else if (gapHorizontal == max) {
+									index = col1[j][k].getIndex();
+									index2 = col1[j][k].getIndex2();
+								}
+								else {
+									index = col2[j-1][k].getIndex();
+									index2 = col2[j-1][k].getIndex2();
+								}
+								col2[j][k] = new Cell(max, index, index2);
+							}	
+
+						} else {
+							int cross = col1[j][k-1].getScore() + match(first.charAt(i-1),'-',third.charAt(k-1));
+							int left = col1[j][k].getScore() + match(first.charAt(i-1),'-','-');
+							int right = col2[j][k-1].getScore() + match('-','-',third.charAt(k-1));
+							int crossCross = col1[j-1][k-1].getScore() + match(first.charAt(i-1),second.charAt(j-1),third.charAt(k-1));
+							int leftCross = col1[j-1][k].getScore() + match(first.charAt(i-1),second.charAt(j-1),'-');
+							int rightCross = col2[j-1][k-1].getScore() + match('-',second.charAt(j-1),third.charAt(k-1));
+							int up = col2[j-1][k].getScore() + match('-',second.charAt(j-1),'-');
+
+							int [] array = {crossCross, cross, left, right, leftCross, rightCross, up};
+							int max = array[0];
+							for ( int m = 1; m < array.length; m++ ) {
+								max = max > array[m] ? max : array[m];
+							}
+							if (i <= x)
+								col2[j][k] = new Cell(max, j, k);
+							else {
+								if (crossCross == max) {
+									col2[j][k] = new Cell(max, col1[j-1][k-1].getIndex(), col1[j-1][k-1].getIndex2());
+								}
+								else if (cross == max) {
+									col2[j][k] = new Cell(max, col1[j][k-1].getIndex(), col1[j][k-1].getIndex2());
+								}
+								else if (left == max) {
+									col2[j][k] = new Cell(max, col1[j][k].getIndex(), col1[j][k].getIndex2());
+								}
+								else if (right == max) {
+									col2[j][k] = new Cell(max, col2[j][k-1].getIndex(), col2[j][k-1].getIndex2());
+								}
+								else if (leftCross == max) {
+									col2[j][k] = new Cell(max, col1[j-1][k].getIndex(), col1[j-1][k].getIndex2());
+								}
+								else if (rightCross == max) {
+									col2[j][k] = new Cell(max, col2[j-1][k-1].getIndex(), col2[j-1][k-1].getIndex2());
+								}
+								else { // up
+									col2[j][k] = new Cell(max, col2[j-1][k].getIndex(), col2[j-1][k].getIndex2());
+								}
+							}
+						}
+					}
+				}
+				// Swap cols
+				col1 = col2.clone();
+			}
+			
+			// Generate substrings
+			int y = col2[rows-1][depth-1].getIndex();
+			int z = col2[rows-1][depth-1].getIndex2();
+			String sub1L = first.substring(0, x);
+			String sub1R = first.substring(x,first.length());
+			String sub2L = second.substring(0, y);
+			String sub2R = second.substring(y,second.length());
+			String sub3L = third.substring(0, z);
+			String sub3R = third.substring(z,third.length());
+			findSimilarity(sub1L, sub2L, sub3L, offsetX, offsetY, offsetZ);
+			findSimilarity(sub1R, sub2R, sub3R, offsetX+x, offsetY+y, offsetZ+z);
 		}
-
-
-		return;
 	}
 
 	// Zwraca karę/nagrodę za dopasowanie dwóch znaków
-	private int match( char a, char b ) {
+	private int match(char a, char b) {
 		int row = charToIndex(a);
 		int col = charToIndex(b);
 		return model.similarityMatrix[row][col];
 	}
 
 	// Zwraca karę/nagrodę za dopasowanie trzech znaków
-	private int match( char a, char b, char c ) {
+	private int match(char a, char b, char c) {
 		int row = charToIndex(a);
 		int col = charToIndex(b);
 		int dep = charToIndex(c);
@@ -186,16 +326,16 @@ public class ControllerClean {
 	// Przyjmuje znak i zwraca odpowiadający mu indeks w macierzy kar i nagród
 	private int charToIndex(char a) {
 		int index;
-		if ( a == 'A' ) {
+		if (a == 'A') {
 			index = 0;
 		}
-		else if ( a == 'G' ) {
+		else if (a == 'G') {
 			index = 1;
 		}
-		else if ( a == 'C' ) {
+		else if (a == 'C') {
 			index = 2;
 		}
-		else if ( a == 'T' ) {
+		else if (a == 'T') {
 			index = 3;
 		}
 		else
@@ -205,7 +345,7 @@ public class ControllerClean {
 
 	public static void main(String[] args) {
 		ControllerClean c = new ControllerClean();
-		c.findSimilarity("GAAA", "GCAAA", "GCAAA", 0, 0, 0);
+		c.findSimilarity("GCAAAAAAAAAAAAA", "GCAAAAAAAAAAAA", "GCAAAAAAAAAAAA", 0, 0, 0);
 		for ( Tuple t : c.result ) {
 			System.out.println(t);
 		}
